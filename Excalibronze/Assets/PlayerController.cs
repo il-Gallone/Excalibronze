0efd5b0ce33fdbbf39b582845b2bdc80de9f9971
Ian Kirk <iankirk@live.com>
2018-06-22 14:16:04 +1000

    Player Controller Version 2

--- Excalibronze/Assets/PlayerController.cs
+++ Excalibronze/Assets/PlayerController.cs
@@ -18,22 +18,22 @@ public class PlayerController : MonoBehaviour {
     }
 	
 	void FixedUpdate () {
-        if (Input.GetAxis("Vertical") <= -0.5)
+        if (Input.GetAxis("Vertical") <= -0.2)
         {
             transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
             spriteRenderer.sprite = down;
         }
-        if (Input.GetAxis("Vertical") >= 0.5)
+        if (Input.GetAxis("Vertical") >= 0.2)
         {
             transform.position += new Vector3(0, speed * Time.deltaTime, 0);
             spriteRenderer.sprite = up;
         }
-        if (Input.GetAxis("Horizontal") <= -0.5)
+        if (Input.GetAxis("Horizontal") <= -0.2)
         {
             transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
             spriteRenderer.sprite = left;
         }
-        if (Input.GetAxis("Horizontal") >= 0.5)
+        if (Input.GetAxis("Horizontal") >= 0.2)
         {
             transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
             spriteRenderer.sprite = right;
