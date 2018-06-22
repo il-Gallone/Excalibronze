575cbe0a72d31b9d8a1ed458ce9c82520cba9d57
Ian Kirk <iankirk@live.com>
2018-06-22 13:58:31 +1000

    First Player Movement Program

---
+++ Excalibronze/Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float speed = 3.0F;
+    public Sprite up;
+    public Sprite right;
+    public Sprite down;
+    public Sprite left;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+	
+	void FixedUpdate () {
+        if (Input.GetAxis("Vertical") <= -0.5)
+        {
+            transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
+            spriteRenderer.sprite = down;
+        }
+        if (Input.GetAxis("Vertical") >= 0.5)
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+            spriteRenderer.sprite = up;
+        }
+        if (Input.GetAxis("Horizontal") <= -0.5)
+        {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+            spriteRenderer.sprite = left;
+        }
+        if (Input.GetAxis("Horizontal") >= 0.5)
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            spriteRenderer.sprite = right;
+        }
+    }
+}
